0edabe4278cd00e78dcc5721634db04ad5977e54
Eivin Landa <landa@drivdigital.no>
2017-09-17 15:09:42 +0200

    Copied Daemon from the chip

---
+++ Daemon.php
@@ -0,0 +1,69 @@
+<?php
+error_reporting(E_ALL);
+
+/* Allow the script to hang around waiting for connections. */
+set_time_limit(0);
+
+/* Turn on implicit output flushing so we see what we're getting
+ * as it comes in. */
+ob_implicit_flush();
+
+$address = '192.168.1.53';
+$address = '127.0.0.1';
+$port = 10001;
+
+/**
+ * Create socket stuff
+ */
+if ( ( $sock = socket_create( AF_INET, SOCK_STREAM, SOL_TCP ) ) === false ) {
+    echo "socket_create() failed: reason: " . socket_strerror(socket_last_error()) . "\n";
+}
+if (socket_bind($sock, $address, $port) === false) {
+    echo "socket_bind() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+}
+if (socket_listen($sock, 5) === false) {
+    echo "socket_listen() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+}
+
+/**
+ * Forever loop to listen to the socket
+ */
+while ( 1 ) {
+    $accept = $msgsock = socket_accept($sock);
+    if ( $accept === false) {
+        echo "socket_accept() failed: reason: " . socket_strerror(socket_last_error($sock)) . "\n";
+        break;
+    }
+    /* Send instructions. */
+    $msg = "Server started\n";
+    socket_write($msgsock, $msg, strlen($msg));
+
+    while ( true ) {
+        $buffer = @socket_read( $msgsock, 2048, PHP_NORMAL_READ );
+        if ( false === $buffer ) {
+            $error = socket_last_error( $msgsock );
+            if ( 104 !== $error ) {
+                echo "socket_read() failed: reason: ($error) " . socket_strerror( $error ) . "\n";
+                break 2;
+            } else {
+                // echo "Socket disconnected";
+                break 1;
+            }
+        }
+        if (!$buffer = trim($buffer)) {
+            continue;
+        }
+        // $talkback = "PHP: You said '$buffer'.\n";
+        // socket_write( $msgsock, $talkback, strlen($talkback) );
+        // echo "$buffer\n";
+        if ( ! preg_match('/^\d:\d$/', $buffer) ) {
+            echo "Invalid command sent: $buffer\n";
+            continue;
+        }
+        echo "Sending to TTY: $buffer\n";
+        shell_exec( __DIR__.'/light '. $buffer );
+
+    }
+    socket_close($msgsock);
+}
+socket_close($sock);
